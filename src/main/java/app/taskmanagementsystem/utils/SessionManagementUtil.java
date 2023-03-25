7dc2cddecaff3344b45688948459bfb24715932c
RosenMitrov <rdatabase.m@gmail.com>
2023-03-25 10:44:05 +0200

    Added SessionManagementUtil class to log out user in current session.

---
+++ src/main/java/app/taskmanagementsystem/utils/SessionManagementUtil.java
@@ -0,0 +1,40 @@
+package app.taskmanagementsystem.utils;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Enumeration;
+
+public class SessionManagementUtil {
+
+    public static void logoutUser(HttpServletRequest httpServletRequest) throws ServletException {
+
+        httpServletRequest.logout();
+
+        SecurityContextHolder.getContext().setAuthentication(null);
+        SecurityContextHolder.clearContext();
+
+        HttpSession httpSession = httpServletRequest.getSession();
+        Enumeration<String> attributeNames = httpSession.getAttributeNames();
+
+        while (attributeNames.hasMoreElements()) {
+            String attr = attributeNames.nextElement();
+            httpSession.setAttribute(attr, null);
+        }
+
+        removeCookies(httpServletRequest);
+        httpSession.invalidate();
+    }
+
+    private static void removeCookies(HttpServletRequest httpServletRequest) {
+        Cookie[] allCookies = httpServletRequest.getCookies();
+        if (allCookies != null && allCookies.length > 0) {
+            for (Cookie cookie : allCookies) {
+                cookie.setMaxAge(0);
+            }
+        }
+    }
+}
