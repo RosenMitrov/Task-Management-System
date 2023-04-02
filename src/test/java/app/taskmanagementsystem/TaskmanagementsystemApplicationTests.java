c07a8d94ba5bd3c5918ded73c3fe9f86f55c0ca1
RosenMitrov <rdatabase.m@gmail.com>
2023-04-02 15:57:18 +0300

    .

--- src/test/java/app/taskmanagementsystem/TaskmanagementsystemApplicationTests.java
+++ src/test/java/app/taskmanagementsystem/TaskmanagementsystemApplicationTests.java
@@ -1,11 +1,16 @@
 package app.taskmanagementsystem;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class TaskmanagementsystemApplicationTests {
 
+    @Value("${mail.host}") String mailHost;
+    @Value("${mail.port}") Integer mailPort;
+    @Value("${mail.username}") String mailUsername;
+    @Value("${mail.password}") String mailPassword;
     @Test
     void contextLoads() {
     }
